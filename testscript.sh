5d878b021a37b50fd2317dcecb10f7ced71bf5b9
nisha175 <nishasingh3127@gmail.com>
2022-05-27 17:02:40 +0530

    uodated script file

--- testscript.sh
+++ testscript.sh
@@ -1,9 +1,9 @@
-#!/bin/bash
+#! /bin/bash
 container_count=$1
 echo "creating $container_count containers "
 sleep 2;
 
-for i in 'seq $container_count'
+for i in `seq $container_count`
 do
  echo "creating conatiner_$i"
  docker run -it -d --name container_$i testcontainerimg /bin/bash
