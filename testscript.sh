c18802d9b52f695cb8b84fc69c7af6102a2f2253
nisha175 <nishasingh3127@gmail.com>
2022-05-27 17:39:01 +0530

    updated script

--- testscript.sh
+++ testscript.sh
@@ -3,7 +3,7 @@ container_count=$1
 echo "creating $container_count containers "
 sleep 2;
 
-for i in `seq $container_count`
+for i in `1 2 3 4`
 do
  echo "creating conatiner_$i"
  docker run -it -d --name container_$i testcontainerimg /bin/bash
