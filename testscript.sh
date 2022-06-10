5f1696046b4e35817cfef7f9299d0d33c2a636c3
nisha175 <89409716+nisha175@users.noreply.github.com>
2022-06-10 11:05:30 +0530

    Update testscript.sh

--- testscript.sh
+++ testscript.sh
@@ -1,9 +1,10 @@
-container_count=$1
+#!/bin/bash
+#container_count=$1
 echo "container creation starts"
 
 for i in {1..5}
 do
  echo "creating container: $i"
- docker run -itd --name testcon_$i ubuntu /bin/bash
+ docker run -itd --name testcon_$i tomcatimg /bin/bash
  echo "created container: testcon_$i "
 done
